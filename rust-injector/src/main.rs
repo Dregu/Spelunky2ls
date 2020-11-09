bdd136dc487f1d10831db35ea2ebb75b922f6c6b
Dregu <dregu@dreg.us>
2020-11-09 02:07:00 +0200

    clean

--- rust-injector/src/main.rs
+++ rust-injector/src/main.rs
@@ -1,25 +1,6 @@
 mod injector;
 use injector::*;
 
-/*use std::net::UdpSocket;
-use std::str;
-use std::str::FromStr;
-fn main() {
-    env_logger::Builder::new()
-        .filter(None, log::LevelFilter::Debug)
-        .init();
-    let socket = UdpSocket::bind("127.0.0.1:5001").unwrap();
-    loop {
-        let mut buf = [0; 16];
-        let (amt, src) = socket.recv_from(&mut buf).unwrap();
-        let msg = str::from_utf8(&buf).unwrap();
-        let numbers: Vec<String> = msg.split_whitespace().map(|s| s.parse().unwrap()).collect();
-        let id: usize = numbers[0].parse().unwrap();
-        let dx: f32 = numbers[1].parse().unwrap();
-        let dy: f32 = numbers[2].parse().unwrap();
-    }
-}*/
-
 fn main() {
     env_logger::Builder::new()
         .filter(None, log::LevelFilter::Debug)
